81d784d53c7df1c75bbf0bf80b14e7d3fbdc39ee
Neil Johnson <neil.johnson@erudicon.com>
2023-02-05 12:57:45 -0600

    Added Contrl Vars

--- banana_survey.py
+++ banana_survey.py
@@ -19,35 +19,40 @@ title = tk.Label(
           fg='#FF0'
 )
 
+name_var = tk.StringVar(root)
 name_label = tk.Label(root, text='What is your name?')
-name_inp = tk.Entry(root)
+name_inp = tk.Entry(root, textvariable=name_var)
 
+eater_var = tk.BooleanVar()
 eater_inp = tk.Checkbutton(
     root,
+    variable=eater_var,
     text='Check this box if you eat bananas'
 )
 
+num_var = tk.IntVar(value=3)
 num_label = tk.Label(
     root,
     text='How many bananas do you eat per day?'
 )
-num_inp = tk.Spinbox(root, from_=0.0, to=1000, increment=1)
+num_inp = tk.Spinbox(root, textvariable=num_var, from_=0, to=1000, increment=1)
 
+color_var = tk.StringVar(value='any')
 color_label = tk.Label(
     root,
     text='What is the best color for a banana?'
 )
-color_inp = tk.Listbox(root, height=1)
 color_choices=('any', 'Green', 'Green-Yellow',
                'Yellow', 'Brown Spotted', 'Black'
                )
-for choice in color_choices:
-    color_inp.insert(tk.END, choice)
+color_inp = tk.OptionMenu(
+    root, color_var, *color_choices)
 
 plantain_label = tk.Label(root, text='Do you east plantains?')
 plantain_frame = tk.Frame(root)
-plantain_yes_inp = tk.Radiobutton(plantain_frame, text='Yes')
-plantain_no_inp = tk.Radiobutton(plantain_frame, text='Eww, no!')
+plantain_var = tk.BooleanVar()
+plantain_yes_inp = tk.Radiobutton(plantain_frame, text='Yes', value=True, variable=plantain_var)
+plantain_no_inp = tk.Radiobutton(plantain_frame, text='Eww, no!', value=False, variable=plantain_var)
 
 banana_haiku_label = tk.Label(
     root,
@@ -57,7 +62,8 @@ banana_haiku_inp = tk.Text(root, height=3)
 
 submit_btn = tk.Button(root, text='Submit Survey')
 
-output_line = tk.Label(root, text='', anchor='w', justify='left')
+output_var = tk.StringVar(value='')
+output_line = tk.Label(root, textvariable=output_var, anchor='w', justify='left')
 
 title.grid(columnspan=2)
 
@@ -91,23 +97,34 @@ root.rowconfigure(100, weight=1)
 def on_submit():
     """To be run when the user submits the form"""
     
-    name = name_inp.get()
-    number = num_inp.get()
+    name = name_var.get()
 
-    selected_idx = color_inp.curselection()
-    if selected_idx:
-        color = color_inp.get(selected_idx)
-    else:
-        color=''
+    try:
+        number = num_var.get()
+    except tk.TclError:
+        number = 10000
+
+    color = color_var.get()
+    banana_eater = eater_var.get()
+    plantain_eater = plantain_var.get()
 
     haiku = banana_haiku_inp.get('1.0', tk.END)
 
-    message = (
-        f'Thanks for taking the survey {name}.\n'
-        f'Enjoy your {number} {color} bananas!\n'
-    )
-    output_line.config(text=message)
-    print(haiku)
+    message = f'Thanks for taking the survey {name}.\n'
+    if not banana_eater:
+        message += "Sorry you don't like bananas!\n"
+    else:
+        message += f'Enjoy your {number} {color} bananas!\n'
+    
+    if plantain_eater:
+        message += 'Enjoy your plantains!'
+    else:
+        message += 'May successfully avoid plantains!'
+
+    if haiku.strip():
+        message += f'\n\nYour Haiku:\n{haiku}'
+
+    output_var.set(message)
 
 submit_btn.configure(command=on_submit)
 
