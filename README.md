959234f1834f083ed6f25f7e6c0725334f9c5572
Neil Johnson <neil.johnson@erudicon.com>
2023-02-09 16:46:25 -0600

    Ready to add heights (pg 71)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-tkinter
+
 Experiments with python and tkinter
--- data_entry_ap.py
+++ data_entry_ap.py
@@ -96,3 +96,29 @@ ttk.Checkbutton(
     text='Equipment Fault'
 ).grid(row=2, column=0, sticky=tk.W, pady=5)
 
+p_info = ttk.LabelFrame(drf, text="Plant Data")
+p_info.grid(sticky=(tk.W + tk.E))
+for i in range(3):
+    p_info.columnconfigure(i, weight=1)
+
+variables['Plants'] = tk.IntVar()
+ttk.Label(p_info, text='Plants').grid(row=0, column=0)
+ttk.Spinbox(
+    p_info, textvariable=variables['Plants'],
+    from_=0, to=20, increment=1
+).grid(row=1, column=1, sticky=(tk.W + tk.E))
+
+variables['Blossoms'] - tk.IntVar()
+ttk.Label(p_info, text='Blossoms').grid(row=0, column=1)
+ttk.Spinbox(
+    p_info, textvariable=variables['Blossoms'],
+    from_=0, to=1000, increment=1
+).grid(row=1, column=1, sticky=(tk.W + tk.E))
+
+variables['Fruit'] = tk.IntVar()
+ttk.Label(p_info, text='Fruit').grid(row=0, column=1)
+ttk.Spinbox(
+    p_info, textvariable=variables['Fruit'],
+    from_=0, to=1000, increment=1
+).grid(row=1,column=1, sticky=(tk.W + tk.E))
+
